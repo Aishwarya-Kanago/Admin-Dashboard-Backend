09a75eee33a220bec0ece135f35505455fb70133
Aishwarya-Kanago <aishwaryakanago21@gmail.com>
2024-12-25 22:17:09 +0530

    build file changes 2

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,6 @@
  python3.12 -m venv env
  source env/bin/activate
  echo "Virtual ENV ACTIVATED"
- sudo apt-get install python-dev default-libmysqlclient-dev
- sudo apt-get install python3-dev
  python3.12 -m pip install -r requirements.txt
  python3.12 manage.py collectstatic
  echo "BUILD END"
