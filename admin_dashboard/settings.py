2e8542734b2efb8052c3f2d853bd18c2e3427af0
Aishwarya-Kanago <aishwaryakanago21@gmail.com>
2024-12-26 20:22:11 +0530

    changes in config

--- admin_dashboard/settings.py
+++ admin_dashboard/settings.py
@@ -26,18 +26,12 @@ SECRET_KEY = 'django-insecure-ayx+lr%hno6+axwbs)yq-_$(%b_yp(0e!huaf#omaiqhakpqkz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "aishwarya-kanago.github.io",  # Remove protocol
-    "localhost",
-    "127.0.0.1",
-    "admin-dashboard-backend-p5ussukag-aishwaryas-projects-232113c3.vercel.app",
-    "admin-dashboard-backend-gray.vercel.app",
-]
+ALLOWED_HOSTS = ['*']
 
 # Allow all origins OR specify allowed origins (not both)
-CORS_ALLOW_ALL_ORIGINS = False  # Set to True only if you want to allow all origins (NOT recommended in production)
+CORS_ORIGIN_ALLOW_ALL = True  # Set to True only if you want to allow all origins (NOT recommended in production)
 
-CORS_ALLOWED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1",
     "http://localhost:5173",
     "https://aishwarya-kanago.github.io",
@@ -56,13 +50,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     "users",
     "product",
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'admin_dashboard.urls'
@@ -158,3 +152,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
